2bcdf00a1a7342fd647c9f63c6302e2197481851
Taseen Jeem <taseenjeem.dev@gmail.com>
2024-07-28 01:50:52 +0600

    added navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ReduxStoreProvider>
-        <Navbar />
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Navbar />
+          {children}
+        </body>
       </ReduxStoreProvider>
     </html>
   );
--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -4,20 +4,24 @@ const Navbar = () => {
   return (
     <nav className="w-full p-5 bg-gray-300 fixed top-0">
       <div className="w-full flex justify-center">
-        <div className="flex space-x-10">
-          <Link
-            href={`/`}
-            className="bg-blue-300 py-2 px-3 rounded-md hover:bg-blue-900 hover:text-white duration-300"
-          >
-            Add a human
-          </Link>
-          <Link
-            href={`/`}
-            className="bg-blue-300 py-2 px-3 rounded-md hover:bg-blue-900 hover:text-white duration-300"
-          >
-            Show all humans
-          </Link>
-        </div>
+        <ul className="flex space-x-10">
+          <li>
+            <Link
+              href={`/`}
+              className="bg-blue-300 py-2 px-3 rounded-md hover:bg-blue-900 hover:text-white duration-300"
+            >
+              Add a human
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={`/`}
+              className="bg-blue-300 py-2 px-3 rounded-md hover:bg-blue-900 hover:text-white duration-300"
+            >
+              Show all humans
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
